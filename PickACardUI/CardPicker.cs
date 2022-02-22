e480ef8a19e1c7f32111f4effe569c3235428d39
nowak90 <imarcin.nowaczyk@gmail.com>
2022-02-22 23:25:46 +0100

    Add project files.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for(int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // getting number 1-4
+            int value = random.Next(1, 5);
+            //if number is 1, then 'clubs' is returned
+            if (value == 1) return "of clubs";
+            if (value == 2) return "of hearts";
+            if (value == 3) return "of spades";
+            //if until here the method did not return, it should return diamonds
+            return "of diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
